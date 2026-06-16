2ce814e4a1bfe57d89b4256421c8df3ae728c9ca
MubashirD04 <144245666+MubashirD04@users.noreply.github.com>
2026-06-16 11:09:35 +0200

    Restructered with Astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://mubashird04.github.io',
+    base: '/Portfolio/',
+});
